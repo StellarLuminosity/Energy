a342eeecc3952cf5a88d8f6683780e9727304cb0
Katherine Lambert <k.lambertcanada@gmail.com>
2026-01-23 09:11:14 -0500

    updated scripts

--- run_kd_32b_to_13b.sh
+++ run_kd_32b_to_13b.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=kd_distill_13b
+#SBATCH --job-name=nosft_kd_distill_13b
 #SBATCH --output=/scratch/klambert/run_logs/%x_%j.out                
 #SBATCH --error=/scratch/klambert/run_logs/%x_%j.err                                            
-#SBATCH --partition=gpubase_h100_b2
+#SBATCH --partition=gpubase_h100_b5
 #SBATCH --gres=gpu:h100:1
 #SBATCH --cpus-per-task=16                                                                
 #SBATCH --mem=120GB
 #SBATCH --export=NONE
-#SBATCH --account=aip-craffel  
-#SBATCH --exclude=kn174                                  
-#SBATCH --time=12:00:00
+#SBATCH --account=aip-craffel                      
+#SBATCH --time=4-00:30:00
 
 # Unified experiment launcher for KD/SFT/DPO pipelines (single-GPU)
 # 1 H100:        srun -c 16 --gres=gpu:h100:1 --partition=gpubase_h100_b5 --mem=120GB --pty --time=3:00:00 --account=aip-craffel bash
